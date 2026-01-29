c165da82d4e57efe5400f83f5ada3ff4e4e0389a
MohamedOsmanX <hamarsaysaab1@gmail.com>
2026-01-29 20:31:08 +0400

    still building the Ui

--- lib/movies/presentation/screens/movie_screen.dart
+++ lib/movies/presentation/screens/movie_screen.dart
@@ -1,10 +1,11 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movies_app/core/services/services_locator.dart';
-import 'package:movies_app/movies/domain/usecases/get_now_playing_movies.dart';
-import 'package:movies_app/movies/presentation/controllers/movies_bloc.dart';
-import 'package:movies_app/movies/presentation/controllers/movies_event.dart';
-import 'package:movies_app/movies/presentation/controllers/movies_state.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movies_app/core/utils/dummy.dart';
+
+import 'package:shimmer/shimmer.dart';
 
 class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key});
@@ -16,7 +17,47 @@ class MovieScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          
+          FadeIn(
+            duration: const Duration(milliseconds: 500),
+            child: CarouselSlider(
+              options: CarouselOptions(
+              height: 400.0,
+              viewportFraction: 1.0,
+              onPageChanged: (index, reason) {},
+            ),
+            items: moviesList.map(
+              (item) {
+                return GestureDetector(
+                  key: const Key('openMovieMinimalDetail'),
+                  onTap: () {
+                    ///TODO navigate to movie detail
+                  },
+                  child: Stack(
+                    children: [
+                      ShaderMask(
+                        shaderCallback: (rect) {
+                          return const LinearGradient(
+                            begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                // fromLTRB
+                                Colors.transparent,
+                                Colors.black,
+                                Colors.black,
+                                Colors.transparent,
+                              ],
+                              stops: [0, 0.3, 0.5, 1],
+                          ).createShader(Rect.fromLTRB(0, 0.3, 0.5, 1));
+                        },
+                        blendMode: BlendMode.dstIn,
+                        child: Ca,
+                      )
+                    ],
+                  ),
+                )
+              }
+            )
+          ),
         ],
       ),
     );
